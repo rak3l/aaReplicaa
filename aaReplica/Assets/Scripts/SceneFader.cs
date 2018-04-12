bcdd6e6698464470f43d13ecd05e307809c54a05
rak3l <32982569+rak3l@users.noreply.github.com>
2018-04-12 17:15:06 +0200

    level selector script
    did a level selector script
    did a fader
    sort order 9999 as the levels where coming over the fader

---
+++ aaReplica/Assets/Scripts/SceneFader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneFader : MonoBehaviour{
+
+    public Image img;
+    public AnimationCurve curve;
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn ()
+    {
+        float t = 1f; 
+
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color (0f, 0f, 0f, a);
+            yield return 0;
+        }
+    }
+
+
+}

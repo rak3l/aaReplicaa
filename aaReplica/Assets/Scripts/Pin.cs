7d3a06ca446b7d30644b5d4f89125ecd19c6f73d
rak3l <32982569+rak3l@users.noreply.github.com>
2018-04-13 17:25:14 +0200

    pin
    pin goes uppppp

--- aaReplica/Assets/Scripts/Pin.cs
+++ aaReplica/Assets/Scripts/Pin.cs
@@ -2,41 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pin : MonoBehaviour {
+public class Pin : MonoBehaviour
+{
 
     #region properties
-    private bool isPinned = false;
 
     public float speed = 20f;
     public Rigidbody2D rb;
-    #endregion
 
-    #region methods
-    /*void Update()
+    void Update()
     {
-        if (!isPinned)
-            rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
-    }*/
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
-    }
-
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.collider.tag == "Rotator")
-        {
-            transform.SetParent(col.transform);
-            isPinned = true;
-            rb.bodyType = RigidbodyType2D.Kinematic;
-        }
-        else
-        {
-
-        }
-        
+        rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
     }
 
     #endregion

081a05cfedd6ae0cbf0d9951818c157307a44700
rak3l <32982569+rak3l@users.noreply.github.com>
2018-04-13 11:26:50 +0200

    ayee
    Created button functions to go to the levels with fader

--- aaReplica/Assets/Scripts/LevelSelector.cs
+++ aaReplica/Assets/Scripts/LevelSelector.cs
@@ -2,10 +2,10 @@
 
 public class LevelSelector : MonoBehaviour {
 
-   // public SceneFader fader;
+   public SceneFader fader;
 
-    //public void Select (string levelName)
-   // {
-     //   fader.FadeTo(LevelName);
-    //}
+    public void Select(string levelName)
+    {
+        fader.FadeTo(levelName);
+    }
 }
